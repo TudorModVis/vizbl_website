8bfc30f49662ffd1e8bdea50f2a88d674f24529c
Cebotarenco Tudor <tudorcebotarenco@gmail.com>
2024-02-06 19:55:20 +0200

    reset password page

--- app/reset-password/[token]/page.tsx
+++ app/reset-password/[token]/page.tsx
@@ -1,7 +1,8 @@
 'use client'
 
-import { useState } from "react"
-import PasswordField from "./PasswordField"
+import { useEffect, useState } from "react"
+import PasswordField from "./PasswordField";
+import LinkState from "@/app/shared/LinkState";
 
 export default function Page({ params }: { params: { token: string } }) {
     const [newPassword, setNewPassword] = useState('');
@@ -9,6 +10,15 @@ export default function Page({ params }: { params: { token: string } }) {
 
     const [newPasswordError, setNewPasswordError] = useState<string | null>(null);
     const [confirmPasswordError, setConfirmPasswordError] = useState<string | null>(null);
+
+    const [pageContent, setPageContent] = useState<string | null>(null);
+
+    useEffect(() => {
+        fetch("https://server.studiomodvis.com/api/reset-password?token=" + params.token)
+        .then(res => {
+           res.ok ? setPageContent('password') : setPageContent('invalid');
+        });
+    }, []);
 
     const checkNewPassword = () => {
         if (newPassword === '') {
@@ -45,21 +55,19 @@ export default function Page({ params }: { params: { token: string } }) {
             },
             credentials: 'include',
             body: JSON.stringify({
-                token: params.token
+                token: params.token,
+                password: confirmPassword
             }),
         })
-        .then(res => res.json())
-        .then(data => {
-            if (data.id !== undefined) {
-                return;
+        .then(res => {
+            if (res.ok) {
+                setPageContent('success');
             }
-        });
+        })
     }
 
-    return(
-        <div className="w-screen h-screen relative flex justify-center items-center">
-            <img src="/images/bg.png" alt="background" className="absolute left-0 top-0 w-full h-full object-cover -z-10"/>
-            <img src="/images/logo.png" alt="logo" className="absolute left-[4.5rem] top-12 w-[8.3vw] max-w-40" />
+    const PasswordPanel = () => {
+        return (
             <div className="bg-semi-black rounded-md border border-gray w-[40rem] flex flex-col items-center pt-6 pb-12 px-16">
                 <img src="/images/models/lock.png" alt="lock" className="w-60" />
                 <p className="font-bold text-5xl leading-[140%] mb-6">RESET PASSWORD</p>
@@ -68,6 +76,25 @@ export default function Page({ params }: { params: { token: string } }) {
                 <PasswordField label="Confirm New Password*" password={confirmPassword} setPassword={setConfirmPassword} error={confirmPasswordError} setError={setConfirmPasswordError}/>
                 <button className="mt-[1.7rem] py-4 w-44 colored-button border border-transparent hover:border-white font-bold" onClick={sendLoginData}>Continue</button>
             </div>
+        )
+    }
+
+    let contentToLoad = <PasswordPanel />;
+
+    switch (pageContent) {
+        case 'invalid': contentToLoad = <LinkState title="THE LINK HAS EXPIRED" text="Your verification link has expired, if you still want to reset your password send another link." succes={false} />; break;
+        case 'success': contentToLoad = <LinkState title="YOUR NEW PASSWORD IS SET" text="Your password has been successfully modified. Enjoy the extension and don’t forget it this time." succes={true} />; break;
+    }
+
+    if (pageContent === null) {
+        return;
+    }
+
+    return(
+        <div className="w-screen h-screen relative flex justify-center items-center">
+            <img src="/images/bg.png" alt="background" className="absolute left-0 top-0 w-full h-full object-cover -z-10"/>
+            <img src="/images/logo.png" alt="logo" className="absolute left-[4.5rem] top-12 w-[8.3vw] max-w-40" />
+            {contentToLoad}
         </div>
     )
 }
---
+++ app/shared/LinkState.tsx
@@ -0,0 +1,21 @@
+interface LinkStateProps {
+    title: string,
+    text: string,
+    succes: boolean
+}
+
+const LinkState: React.FC<LinkStateProps> =  ({title, text, succes}) => {
+    const imageSrc = succes ? "/images/models/tick.gif" : "/images/models/stop.gif";
+    const finalText = succes ? "Open the extension to continue." : "Open the extension to resend the link.";
+
+    return (
+        <div className="w-[36.75rem] flex flex-col items-center">
+            <img src={imageSrc} alt="gif" className="w-60" />
+            <p className="font-bold text-[4rem] leading-[140%] mb-12 text-center">{title}</p>
+            <p className="text-gray text-2xl font-bold leading-[140%] text-center mb-12">{text}</p>
+            <p className="text-2xl font-bold leading-[140%] text-center">{finalText}</p>
+        </div>
+    );
+}
+
+export default LinkState;
